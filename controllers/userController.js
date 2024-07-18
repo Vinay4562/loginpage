1e268a63af78478a56fa8e44e6b6861bdaf4a621
Vinay4562 <manikantivinaykumar@yahoo.com>
2024-07-18 19:45:12 +0530

    Initial commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User'); // Assuming User model is defined
 
+
+
 const login = async (req, res) => {
   const { username, password } = req.body;
   try {
@@ -37,6 +39,31 @@ const login = async (req, res) => {
   }
 };
 
+
+const forgotPassword = async (req, res) => {
+  const { username, email } = req.body;
+
+  try {
+    // Check if user exists with username and email
+    let user = await User.findOne({ username, email });
+    if (!user) {
+      return res.status(400).json({ msg: 'Invalid Credentials' });
+    }
+
+    // Implement your logic for sending a password reset link
+    // This example doesn't include actual email sending logic
+    // You can generate a unique reset token and send it via email
+
+    res.status(200).json({ msg: 'Password reset link has been sent to your email' });
+
+  } catch (err) {
+    console.error('Error during forgot password:', err.message);
+    res.status(500).json({ msg: 'Server error' });
+  }
+};
+
 module.exports = {
-  login
+  register,
+  login,
+  forgotPassword,
 };
